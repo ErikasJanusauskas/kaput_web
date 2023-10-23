b50bcc94097a584dc89b2f6431cec29e120ef9db
pancakeWizard <114063417+pancakeWizard@users.noreply.github.com>
2023-10-23 14:18:34 +0200

    Updated db structure (model)

--- backend/quiz/models.py
+++ backend/quiz/models.py
@@ -3,4 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Questions(models.Model):
-    title = models.CharField('Question', max_length=50)
+    title   = models.CharField('Question', max_length=512),
+    anwer_1 = models.CharField('Answer_1', max_legth=128 ),
+    anwer_2 = models.CharField('Answer_2', max_legth=128 ),
+    anwer_3 = models.CharField('Answer_3', max_legth=128 ),
+    anwer_4 = models.CharField('Answer_4', max_legth=128 ),
+    notes   = models.CharField('Notes'   , max_legth=128 ),
+    picyure = models.CharField('Image'   , max_length=256)
