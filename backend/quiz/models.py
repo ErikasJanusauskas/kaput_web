3310db76e992fe885e362bb8100a76207cf8c7db
pancakeWizard <114063417+pancakeWizard@users.noreply.github.com>
2023-11-10 09:02:34 +0100

    db

--- backend/quiz/models.py
+++ backend/quiz/models.py
@@ -3,11 +3,18 @@ from django.db import models
 # Create your models here.
 
 class Questions(models.Model):
-    title        = models.CharField('Question', max_length=512)
-    answer_1     = models.CharField('Answer_1', max_length=128 )
-    answer_2     = models.CharField('Answer_2', max_length=128 )
-    answer_3     = models.CharField('Answer_3', max_length=128 )
-    answer_4     = models.CharField('Answer_4', max_length=128 )
-    answer_right = models.IntegerField()
-    notes        = models.CharField('Notes'   , max_length=128 )
-    picture      = models.CharField('Image'   , max_length=256)
+    title        = models.TextField     ('Question')
+    answer_1     = models.CharField     ('Answer_1', max_length=128 )
+    answer_2     = models.CharField     ('Answer_2', max_length=128 )
+    answer_3     = models.CharField     ('Answer_3', max_length=128 )
+    answer_4     = models.CharField     ('Answer_4', max_length=128 )
+    answer_right = models.IntegerField  ('Right answer')
+    notes        = models.CharField     ('Notes'   , max_length=128 )
+    picture      = models.TextField     ('Image')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Question'
+        verbose_name_plural = 'Questions'
