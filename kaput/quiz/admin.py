8f7d40a88ab3039e4479daebc948ba53997e2060
pancakeWizard <114063417+pancakeWizard@users.noreply.github.com>
2023-12-06 10:14:16 +0100

    models + admin + admin-account

--- kaput/quiz/admin.py
+++ kaput/quiz/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Quizzes, Questions
 
 # Register your models here.
+admin.site.register(Quizzes)
+admin.site.register(Questions)
--- kaput/quiz/models.py
+++ kaput/quiz/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class Quizzes(models.Model):
+    name         = models.TextField     ('name')
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Quiz'
+        verbose_name_plural = 'Quizzes'
+
+class Questions(models.Model):
+    quiz         = models.ForeignKey    (Quizzes, blank=True, null=True, on_delete=models.CASCADE, name='quiz')
+    name         = models.TextField     ('name')
+    answer_1     = models.TextField     ('answer_1')
+    answer_2     = models.TextField     ('answer_2')
+    answer_3     = models.TextField     ('answer_3')
+    answer_4     = models.TextField     ('answer_4')
+    answer_r     = models.TextField     ('answer_r')
+    img_path     = models.TextField     ('img_path')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Question'
+        verbose_name_plural = 'Questions'
+
