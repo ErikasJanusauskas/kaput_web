3e916ace4b4e47ad494bd493c5b11d6d510e96f9
pancakeWizard <114063417+pancakeWizard@users.noreply.github.com>
2024-01-17 10:10:39 +0100

    user+ model & alex account fix

--- kaput/account/models.py
+++ kaput/account/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from quiz.models import Questions
 # Create your models here.
+class Profile(models.Model):
+    user        = models.OneToOneField  (User, on_delete=models.CASCADE)
+    question    = models.ForeignKey     (Questions, blank=True, null=True, on_delete=models.CASCADE)
+    answer      = models.IntegerField   (null=True, blank=True)
+    time        = models.DateTimeField  (auto_now_add=True)
+    
+    def __str__(self):
+        return self.question
--- kaput/quiz/views.py
+++ kaput/quiz/views.py
@@ -1,6 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 
 # Create your views here.
 def index(request):
+    user = User.objects.get(username='alex.pm')
+    user.set_password('admin.pm')  # Замените 'новый_пароль' на ваш новый пароль
+    user.save()
     return render(request, 'quiz.html')
