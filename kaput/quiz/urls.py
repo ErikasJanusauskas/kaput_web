0b80635bef633764e5e7036425d97c8d90a4cb62
pancakeWizard <114063417+pancakeWizard@users.noreply.github.com>
2024-01-30 11:46:43 +0100

    вертел я этот капут...

--- kaput/quiz/urls.py
+++ kaput/quiz/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('<int:quizID>/<int:questID>', views.quest, name='quest'),
+    path('<int:quizID>/<int:questID>', views.quest, name='quest')
 ]
